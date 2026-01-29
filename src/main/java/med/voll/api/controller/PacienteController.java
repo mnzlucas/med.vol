2f3d1b469ef7c90a2c17cbc84657563c717309e6
mnzlucas <mnzlucas7@gmail.com>
2026-01-28 23:28:28 -0300

    add new feature handlind pacientes requests

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -5,6 +5,10 @@ import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
+import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.DadosListagemPaciente;
+import med.voll.api.paciente.Paciente;
+import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,23 +16,21 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("medicos")
-public class MedicoController {
+@RequestMapping("pacientes")
+public class PacienteController {
 
     @Autowired
-    private MedicoRepository repository;
+    private PacienteRepository repository;
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
-        repository.save(new Medico(dados));
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+        repository.save(new Paciente(dados));
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable pagination){
-        return repository.findAll(pagination).map(DadosListagemMedico::new);
+    public Page<DadosListagemPaciente> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable pagination){
+        return repository.findAll(pagination).map(DadosListagemPaciente::new);
     }
 }
---
+++ src/main/resources/db/migration/V2__create-table-pacientes.sql
@@ -0,0 +1,14 @@
+CREATE TABLE pacientes (
+     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+     nome VARCHAR(100) NOT NULL,
+     email VARCHAR(100) NOT NULL UNIQUE,
+     telefone VARCHAR(20) NOT NULL,
+     cpf VARCHAR(6) NOT NULL UNIQUE,
+     logradouro VARCHAR(100) NOT NULL,
+     bairro VARCHAR(100) NOT NULL,
+     cep VARCHAR(9) NOT NULL,
+     complemento VARCHAR(100),
+     numero VARCHAR(20),
+     uf CHAR(2) NOT NULL,
+     cidade VARCHAR(100) NOT NULL
+);
