6b93cef7301e83b8252639c12810c264e22f063c
mnzlucas <mnzlucas7@gmail.com>
2026-02-03 23:50:09 -0300

    feature: tokens JWT are valited by the application and the only url there's allowed is /login

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -4,7 +4,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.domain.usuario.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,25 +18,30 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Autowired
     private TokenService tokenService;
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // get token from header bearer
         String JWTtoken = getToken(request);
 
-        // Here you can add logic to validate the token
-        var subject =  tokenService.getSubject(JWTtoken);
-
+        if (JWTtoken != null) {
+            var subject =  tokenService.getSubject(JWTtoken);
+            //chech if subject is valid consulting UserRepository and set authentication in security context
+            var user = userRepository.findByLogin(subject);
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
 
         filterChain.doFilter(request, response);
     }
 
     private static String getToken(HttpServletRequest request) {
         String authorizationHeader = request.getHeader("Authorization");
-        //check if header is null throw runtime exception
-        if (authorizationHeader == null ) {
-            throw new RuntimeException("Token JWT header is missing");
+        if (authorizationHeader != null ) {
+            return authorizationHeader.replace("Bearer ", "");
         }
-        return authorizationHeader.replace("Bearer ", "");
+        return null;
     }
 }
