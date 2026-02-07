51634ee55aa4fd71fb06227cc97f4728bf5643ad
Lucas Almeida <mnzlucas7@gmail.com>
2026-02-07 13:00:21 -0300

    Chore/resolve spring dosc incompatibility version4 (#6)
    * chore: downgrade springboot 4 to version 3.5.10
    
    * chore: add Springdoc with bearer key setup

--- src/main/java/med/voll/api/controller/schedulingMedicalAppointmentController.java
+++ src/main/java/med/voll/api/controller/schedulingMedicalAppointmentController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import med.voll.api.domain.scheduling.SchedulingAppointment;
 import med.voll.api.domain.scheduling.SchedulingAppointmentService;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("consultas")
+@SecurityRequirement(name = "bearer-key")
 public class schedulingMedicalAppointmentController {
 
     @Autowired
